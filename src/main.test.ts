ddd8ab034622c98e5fc0cc639d705a2c6c1ef6e6
Eduardo Bouças <mail@eduardoboucas.com>
2023-06-26 19:13:43 +0100

    chore: fix annotation

--- src/main.test.ts
+++ src/main.test.ts
@@ -10,11 +10,11 @@ beforeAll(async () => {
   if (semver.lt(nodeVersion, '18.0.0')) {
     const nodeFetch = await import('node-fetch')
 
-    // @ts-ignore Expected type mismatch between native implementation and node-fetch
+    // @ts-expect-error Expected type mismatch between native implementation and node-fetch
     globalThis.fetch = nodeFetch.default
-    // @ts-ignore Expected type mismatch between native implementation and node-fetch
+    // @ts-expect-error Expected type mismatch between native implementation and node-fetch
     globalThis.Request = nodeFetch.Request
-    // @ts-ignore Expected type mismatch between native implementation and node-fetch
+    // @ts-expect-error Expected type mismatch between native implementation and node-fetch
     globalThis.Response = nodeFetch.Response
     globalThis.Headers = nodeFetch.Headers
   }
