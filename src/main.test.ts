a6573b2acb218e9bb12cdde3b1a83d64214aa864
Eduardo Bouças <mail@eduardoboucas.com>
2023-07-18 15:28:37 +0100

    feat: throw when API returns an error code (#12)
    * feat: throw when API returns an error code
    
    * chore: fix tests
    
    * chore: fix test

--- src/main.test.ts
+++ src/main.test.ts
@@ -93,6 +93,35 @@ describe('get', () => {
     expect(await blobs.get(key)).toBeNull()
   })
 
+  test('Throws when the API returns a non-200 status code', async () => {
+    const fetcher = async (...args: Parameters<typeof globalThis.fetch>) => {
+      const [url, options] = args
+      const headers = options?.headers as Record<string, string>
+
+      expect(options?.method).toBe('get')
+
+      if (url === `https://api.netlify.com/api/v1/sites/${siteID}/blobs/${key}?context=production`) {
+        expect(headers.authorization).toBe(`Bearer ${apiToken}`)
+
+        return new Response(null, { status: 401, statusText: 'Unauthorized' })
+      }
+
+      throw new Error(`Unexpected fetch call: ${url}`)
+    }
+
+    const blobs = new Blobs({
+      authentication: {
+        token: apiToken,
+      },
+      fetcher,
+      siteID,
+    })
+
+    expect(async () => await blobs.get(key)).rejects.toThrowError(
+      'get operation has failed: API returned a 401 response',
+    )
+  })
+
   test('Throws when a pre-signed URL returns a non-200 status code', async () => {
     const fetcher = async (...args: Parameters<typeof globalThis.fetch>) => {
       const [url, options] = args
@@ -242,6 +271,35 @@ describe('set', () => {
 
     await blobs.set(key, value, { ttl })
   })
+
+  test('Throws when the API returns a non-200 status code', async () => {
+    const fetcher = async (...args: Parameters<typeof globalThis.fetch>) => {
+      const [url, options] = args
+      const headers = options?.headers as Record<string, string>
+
+      expect(options?.method).toBe('put')
+
+      if (url === `https://api.netlify.com/api/v1/sites/${siteID}/blobs/${key}?context=production`) {
+        expect(headers.authorization).toBe(`Bearer ${apiToken}`)
+
+        return new Response(null, { status: 401 })
+      }
+
+      throw new Error(`Unexpected fetch call: ${url}`)
+    }
+
+    const blobs = new Blobs({
+      authentication: {
+        token: apiToken,
+      },
+      fetcher,
+      siteID,
+    })
+
+    expect(async () => await blobs.set(key, 'value')).rejects.toThrowError(
+      'put operation has failed: API returned a 401 response',
+    )
+  })
 })
 
 describe('delete', () => {
@@ -280,5 +338,38 @@ describe('delete', () => {
     })
 
     await blobs.delete(key)
+  })
+
+  test('Throws when the API returns a non-200 status code', async () => {
+    const fetcher = async (...args: Parameters<typeof globalThis.fetch>) => {
+      const [url, options] = args
+      const headers = options?.headers as Record<string, string>
+
+      expect(options?.method).toBe('delete')
+
+      if (url === `https://api.netlify.com/api/v1/sites/${siteID}/blobs/${key}?context=production`) {
+        expect(headers.authorization).toBe(`Bearer ${apiToken}`)
+
+        return new Response(null, { status: 401 })
+      }
+
+      if (url === signedURL) {
+        return new Response('Something went wrong', { status: 401 })
+      }
+
+      throw new Error(`Unexpected fetch call: ${url}`)
+    }
+
+    const blobs = new Blobs({
+      authentication: {
+        token: apiToken,
+      },
+      fetcher,
+      siteID,
+    })
+
+    expect(async () => await blobs.delete(key)).rejects.toThrowError(
+      'delete operation has failed: API returned a 401 response',
+    )
   })
 })
