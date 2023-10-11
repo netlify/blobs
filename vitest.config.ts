30bf62a47b7326490300e46a5a91c7f83fdfe773
Lukas Holzer <lukas.holzer@netlify.com>
2023-10-11 16:28:06 +0200

    chore: cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  esbuild: {
-    target: 'esnext',
-  },
   test: {
     include: ['src/**/*.test.ts'],
     testTimeout: 30_000,
