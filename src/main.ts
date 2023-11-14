2d64083d1c67e7601528f999b78cff4b08e48db2
Matt Kane <matt.kane@netlify.com>
2023-11-14 12:07:45 +0000

    feat: export Store types

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
 export { getDeployStore, getStore } from './store_factory.ts'
 export { BlobsServer } from './server.ts'
+export type {
+  Store,
+  StoreOptions,
+  GetWithMetadataOptions,
+  GetWithMetadataResult,
+  ListOptions,
+  ListResultBlob,
+  SetOptions,
+  BlobResponseType,
+} from './store.ts'
